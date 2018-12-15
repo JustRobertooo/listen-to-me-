2a04b44d0f867b01e6bcabb36b45f660d4a4c12b
JustRobertooo <40578875+JustRobertooo@users.noreply.github.com>
2018-12-15 21:22:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,39 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
+
 client.on('ready', () => {
-  console.log('bot online ');
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`الشيء الي تبيه`,)
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-client.login('token')
+
+
+
+
+
+
+
+
+client.login(process.env.USER_TOKEN);
